3a64cb81bcd3efec6167f3d3191b07073c5f98ca
Constanza Gaete <gconstanzabelen@gmail.com>
2024-12-06 22:35:46 -0300

    creacion C lista resv y navprof integracion home

---
+++ src/app/Components/lista-reserva/lista-reserva.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-reserva',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-reserva.component.html',
+  styleUrl: './lista-reserva.component.css'
+})
+export class ListaReservaComponent {
+
+}
---
+++ src/app/Components/navprof/navprof.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navprof',
+  standalone: true,
+  imports: [],
+  templateUrl: './navprof.component.html',
+  styleUrl: './navprof.component.css'
+})
+export class NavprofComponent {
+
+}
